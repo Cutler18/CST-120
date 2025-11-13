28a4133271a5607c59c1c9be6b195904138e1515
Jacob Cutler <cutlerjacob18@gmail.com>
2025-11-13 16:02:06 -0700

    Add files via upload

---
+++ workspaceCST120-Milestone/JS/mysite.js
@@ -0,0 +1,34 @@
+function toTop()
+{
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+}
+
+function loadAbtMe()
+{
+    window.location.href = "aboutMe.html";
+}
+
+function loadContactMe()
+{
+    console.log("Loading Contact Me.");
+}
+
+function loadMedia()
+{
+    window.location.href = "media.html";
+}
+
+function loadMC()
+{
+    console.log("Loading Marine Corps page");
+}
+
+function loadFam()
+{
+    window.location.href = "family.html";
+}
+
+function loadHob()
+{
+    window.location.href = "hobbies.html";
+}
